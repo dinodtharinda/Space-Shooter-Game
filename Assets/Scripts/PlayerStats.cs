d1d4ba7317a8099406cfcdc1167c973de30ca478
dinod tharinda <dinodtharinda2001@gmail.com>
2024-01-31 20:53:16 +0530

    health bar

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private float maxHealth;
+    [SerializeField] private Image healthFill;
     private float health;
     void Start()
     {
         health = maxHealth;
+        healthFill.fillAmount = health / maxHealth;
     }
 
     public void PlayerTakeDamage(float damage)
     {
         health -= damage;
-
+        healthFill.fillAmount = health / maxHealth;
         if (health <= 0)
         {
             Destroy(gameObject);
