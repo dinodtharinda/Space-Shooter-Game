d90b0b941ee3b9860f408335096a294747f61f5b
dinod tharinda <dinodtharinda2001@gmail.com>
2024-02-02 02:21:43 +0530

    add purple enemy

---
+++ Assets/Scripts/EnemySc/PurpleBullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleBullet : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.down * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerStats player = collision.GetComponent<PlayerStats>();
+            player.PlayerTakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemySc/PurpleEnemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleEnemy : Enemy
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform leftCanon;
+    [SerializeField] private Transform rightCanon;
+    [SerializeField] private float speed;
+
+
+    private float shootTimer = 0;
+    [SerializeField] private float shootIntervel;
+
+    void Start()
+    {
+        rb.velocity = Vector2.down * speed;
+    }
+
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        if (shootIntervel <= shootTimer)
+        {
+            Instantiate(bulletPrefab, leftCanon.position, Quaternion.identity);
+            Instantiate(bulletPrefab, rightCanon.position, Quaternion.identity);
+            shootTimer = 0;
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerStats>().PlayerTakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
