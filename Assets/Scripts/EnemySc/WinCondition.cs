69c9a638d3833cd1b062d3d43377c99eb58ab98a
dinod tharinda <dinodtharinda2001@gmail.com>
2024-02-07 17:02:54 +0530

    Game Manager

---
+++ Assets/Scripts/EnemySc/WinCondition.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    private float timer;
+    [SerializeField] private float possibleWinTime;
+    [SerializeField] private GameObject[] spawner;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= possibleWinTime)
+        {
+            for (int i = 0; i < spawner.Length; i++)
+            {
+                spawner[i].SetActive(false);
+            }
+            EndGameManager.endManager.StartResolveSequence();
+            gameObject.SetActive(false);
+            //create a function that will check if the player survived the last spawned enemy/meteor
+                // win or lose screen
+                    //GAME MANAGER
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/PanelController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup cGroup;
+    [SerializeField] private GameObject winScreen;
+    [SerializeField] private GameObject loseScreen;
+
+    void Start()
+    {
+        EndGameManager.endManager.RegisterPanelController(this);
+    }
+
+    public void ActivateWin()
+    {
+        cGroup.alpha = 1;
+        winScreen.SetActive(true);
+    }
+
+    public void ActivateLose()
+    {
+        cGroup.alpha = 1;
+        loseScreen.SetActive(true);
+    }
+
+
+}
